a6dd68d113a3e5316f83c1b617fdd0d0e03d423d
Viola <shiyimengviola@gmail.com>
2023-05-06 21:14:14 +0200

    add Post

---
+++ app/src/Post.js
@@ -0,0 +1,20 @@
+import './Post.css';
+import React, { useState } from 'react';
+
+function Post(props) {
+
+    return (
+    <div className="Post">
+        <span className = "user">User Name :</span>
+        <div className="post-container">
+        {'shdubushnfdxijd'}
+        </div>
+    </div>
+    )
+}
+
+document.body.className = 'Post';
+
+
+export default Post;
+    
